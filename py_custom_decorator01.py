80986dc7ac8b05377f7f10ca80a3e6edf19e840a
Reed Xia <huaqin2005@gmail.com>
2018-04-10 22:53:38 +0800

    checkin changes

--- py_custom_decorator01.py
+++ py_custom_decorator01.py
@@ -5,10 +5,10 @@ import functools
 def check_is_verified_user(user_name):
     def my_decorator_with_arg(func):
         @functools.wraps(func)
-        def check_and_run():
+        def check_and_run(*args, **kwargs):
             print("begin")
             if user_name == "logined_user":
-                func()
+                func(*args, **kwargs)
             else:
                 print("Sorry! You're not logined user!")
             print("end")
@@ -16,8 +16,9 @@ def check_is_verified_user(user_name):
     return my_decorator_with_arg
 
 
-@check_is_verified_user("not_logined_user")
-def show_user_page():
+@check_is_verified_user("logined_user")
+def show_user_page(x, y):
     print("Welcome! User page is launched!")
+    print(x + y)
 
-show_user_page()
+show_user_page(70, 98)
