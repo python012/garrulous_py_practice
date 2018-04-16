97a7c334980fb19f13e67e2f500a3e2503355b90
Reed Xia <huaqin2005@gmail.com>
2018-04-16 11:44:28 +0800

    finally I can use unittest.mock.patch()..

--- py_unittest_05.py
+++ py_unittest_05.py
@@ -1,24 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 from unittest import main
-     
-     
-class Person(object):
-    def __init__(self, name):
-        self.name = name
-        
-    def print_name(self):
-        print('My name is ' + self.name)
-        
-    def print_parents(self):
-        mother = input("Enter mother's name: ")
-        father = input("Enter father's name: ")
-     
-        print("{}'s parents are {} and {}.".format(self.name, mother, father))
-        self.fake_func()
-
-    def fake_func(self):
-        pass
+from person import Person
 
 class FuncTest(TestCase):
     def test_print_parents(self):
@@ -27,11 +10,12 @@ class FuncTest(TestCase):
         with patch('builtins.input') as mocked_input:
             mocked_input.side_effect = ('Jo', 'Lee')
             with patch('builtins.print') as mocked_print:
-                with patch.object(Person, "fake_func") as mocked_fake_func:
-                # with patch('Person.fake_func') as mocked_fake_func:
+                # with patch.object(person.Person, "fake_func") as mocked_fake_func:
+                with patch('person.Person.fake_func') as mocked_fake_func:
                     john.print_parents()
                     mocked_print.assert_called_with("John's parents are Jo and Lee.")
                     mocked_fake_func.assert_called_once()
- 
+
+
 if __name__ == '__main__':
     main()
