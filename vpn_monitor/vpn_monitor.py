db35763df7c735f4bc795120e3d1b0353b82f9f2
Reed Xia <huaqin2005@gmail.com>
2018-11-18 22:47:56 +0800

    vpn_monitor: change to use requests.get()

--- vpn_monitor/vpn_monitor.py
+++ vpn_monitor/vpn_monitor.py
@@ -1,17 +1,35 @@
 #coding: utf-8
 
+#author: Reed Xia(huaqin2005@gmail.com)
+
 import smtplib
 import config
-import urllib.request as urllib2
+import requests
+# import urllib.request as urllib2
 import time
 import base64
+import socket
+import traceback
 
-email_is_sent = False
+
+last_email_sent_time = None
 error_info = None
 catch_exception = False
 
+
 def send_email(text):
-    print("Start to send email - " + get_time())
+    global last_email_sent_time
+    if last_email_sent_time is None:
+        last_email_sent_time = time.localtime()
+    else:
+        now = time.localtime()
+        if now.tm_hour - last_email_sent_time.tm_hour > 2:
+            last_email_sent_time = now # only send out email at the first time or 1 h later
+        else:
+            print("Less than 1 hour since last email alert, no email this time. - " + get_time())
+            return
+
+    print("Try to send email - " + get_time())
     host_server = config.host_server
     subject = config.subject + " - " + get_time()
     to = config.to
@@ -23,27 +41,41 @@ def send_email(text):
             "",
             text))
 
-    server = smtplib.SMTP()
-    server.connect(host_server, config.sent_port)
-    server.starttls()
-    server.login(config.email_account, \
-            base64.b64decode(config.email_pw.decode()).decode())
-    server.sendmail(from_, to, body)
-    server.quit()
-    print("Finish sending email - " + get_time())
+    try:
+        server = smtplib.SMTP()
+        print(host_server)
+        print(body)
+        server.connect(host_server, config.sent_port)
+        server.starttls()
+        server.login(config.email_account, base64.b64decode(config.email_pw.decode()).decode())
+        server.sendmail(from_, to, body)
+        server.quit()
+        print("Finish sending email - " + get_time())
+    except Exception as err:
+        print("Exception on sending email at" + get_time())
+        print(err.args)
+        # raise
 
 
 def connect_to(url):
     try:
-        urllib2.urlopen(url, timeout = 10)
+        # urllib2.urlopen(url, timeout = 10)
+        requests.get(url, timeout=3)
         return True
-    except urllib2.URLError as urlerr:
-        return False
+    # except socket.timeout as timeout_error:
+    #     return False
     except Exception as err:
-        print("Exception - " + get_time())
+        # print("Exception - " + get_time())
+        # print('----------------------------------------------------')
+        # print(err.__dir__())
+        # print('----------------------------------------------------')
+        # traceback.print_tb()
+        # print('----------------------------------------------------')
+        traceback.print_exc()
+        # print('----------------------------------------------------')
         global error_info
         global catch_exception
-        error_info = str(err)
+        error_info = str(traceback.format_stack())
         catch_exception = True
         return False
 
@@ -54,7 +86,9 @@ def check_network(url):
         if not connect_to(url):
             print("[" + str(i) + "] " + "check_network: fail to open " + url + " at " + get_time())
             bad_network += 1
-        time.sleep(5)
+            time.sleep(5)
+        else:
+            break
     if bad_network == 4:
         return False
     else:
@@ -64,27 +98,20 @@ def check_network(url):
 def get_time():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
+
 def get_email_text():
     global catch_exception
     if not catch_exception:        
-        return "VPN Monitor: Google is disconnected at " + get_time()
-        # + "\nConnected to Baidu status: " + str(baidu_status)
+        return "Hello! Google is disconnected at " + get_time()
     else:
         catch_exception = False
-        return "VPN Monitor: Exception is found at " + get_time() + "\n" + error_info
+        return "Hello! Exception is found at " + get_time() + "\n" + error_info
 
 if __name__ == '__main__':
-    time_interval = 30
     print("VPN Monitor is started at " + get_time())
     while True:
-        if not check_network(config.google):
-            #baidu_status = check_network(config.baidu)
-            # global email_is_sent
-            # if not email_is_sent:
+        if not check_network(config.google_url):
             send_email(get_email_text())
-            email_is_sent = True
-            time_interval = 60 * 30
         else:
-            time_interval = 30
             print("Google is ok at " + get_time())
-        time.sleep(time_interval)
+        time.sleep(90)
