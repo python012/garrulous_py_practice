132c4981bc869e46cf132ade7737d4700500461f
Reed Xia <huaqin2005@gmail.com>
2018-11-19 19:35:11 +0800

    refine log, import logging.

--- vpn_monitor/vpn_monitor.py
+++ vpn_monitor/vpn_monitor.py
@@ -5,16 +5,30 @@
 import smtplib
 import config
 import requests
-# import urllib.request as urllib2
 import time
 import base64
-import socket
+import sys
 import traceback
-
+import logging
 
 last_email_sent_time = None
-error_info = None
+error_info = ""
 catch_exception = False
+logger = logging.getLogger("VPN monitor")
+logger.setLevel(level=logging.DEBUG)
+
+# file handler
+file_handler = logging.FileHandler(time.strftime("%Y-%m-%d_%A_%H%M%S", time.localtime()) + '.log')
+file_handler.setLevel(level=logging.DEBUG)
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(log_format)
+logger.addHandler(file_handler)
+
+# stream handler
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(level=logging.DEBUG)
+stream_handler.setFormatter(log_format)
+logger.addHandler(stream_handler)
 
 
 def send_email(text):
@@ -26,57 +40,43 @@ def send_email(text):
         if now.tm_hour - last_email_sent_time.tm_hour > 2:
             last_email_sent_time = now # only send out email at the first time or 1 h later
         else:
-            print("Less than 1 hour since last email alert, no email this time. - " + get_time())
+            logger.info("Less than 1 hour since last email alert, no email this time. - " + get_time())
             return
 
-    print("Try to send email - " + get_time())
+    logger.info("Try to send email - " + get_time())
     host_server = config.host_server
     subject = config.subject + " - " + get_time()
-    to = config.to
-    from_ = config.from_
+    # to = config.to
+    # from_ = config.from_
     body = "\r\n".join((
-            "From: %s" % from_,
-            "To: %s" % to,
+            "From: %s" % config.from_,
+            "To: %s" % config.to,
             "Subject: %s" % subject,
             "",
             text))
 
     try:
         server = smtplib.SMTP()
-        print(host_server)
-        print(body)
         server.connect(host_server, config.sent_port)
         server.starttls()
         server.login(config.email_account, base64.b64decode(config.email_pw.decode()).decode())
-        server.sendmail(from_, to, body)
+        server.sendmail(config.from_, config.to, body)
         server.quit()
-        print("Finish sending email - " + get_time())
+        logger.info("Finish sending email - " + get_time())
     except Exception as err:
-        print("Exception on sending email at" + get_time())
-        print(err.args)
+        logger.error("Exception on sending email at" + get_time())
+        logger.error(err.args)
         # raise
 
 
 def connect_to(url):
     try:
-        # urllib2.urlopen(url, timeout = 10)
-        requests.get(url, timeout=3)
+        requests.get(url, timeout=10)
         return True
-    # except socket.timeout as timeout_error:
-    #     return False
     except Exception as err:
-        # print("Exception - " + get_time())
-        # print('----------------------------------------------------')
-        # print(err.__dir__())
-        # print('----------------------------------------------------')
-        # traceback.print_tb()
-        # print('----------------------------------------------------')
-        traceback.print_exc()
-        # print('----------------------------------------------------')
+        logger.error(err.args)
         global error_info
-        global catch_exception
-        error_info = str(traceback.format_stack())
-        catch_exception = True
+        error_info = err.args
         return False
 
 
@@ -84,7 +84,7 @@ def check_network(url):
     bad_network = 0
     for i in range(4):
         if not connect_to(url):
-            print("[" + str(i) + "] " + "check_network: fail to open " + url + " at " + get_time())
+            logger.warning("[" + str(i) + "] " + "check_network: fail to open " + url + " at " + get_time())
             bad_network += 1
             time.sleep(5)
         else:
@@ -96,22 +96,19 @@ def check_network(url):
 
 
 def get_time():
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    return time.strftime("%Y-%m-%d %A %H:%M:%S", time.localtime()) #2018-11-19 Monday 18:59:12
 
 
 def get_email_text():
-    global catch_exception
-    if not catch_exception:        
-        return "Hello! Google is disconnected at " + get_time()
-    else:
-        catch_exception = False
-        return "Hello! Exception is found at " + get_time() + "\n" + error_info
+    return "Hello! Google is disconnected at " + get_time() + "\n\nException info:\n\n" + str(error_info)
+
 
 if __name__ == '__main__':
-    print("VPN Monitor is started at " + get_time())
+    logger.info("VPN Monitor is started at " + get_time())
     while True:
         if not check_network(config.google_url):
             send_email(get_email_text())
+            error_info = ""
         else:
-            print("Google is ok at " + get_time())
+            logger.info("Google is ok at " + get_time())
         time.sleep(90)
